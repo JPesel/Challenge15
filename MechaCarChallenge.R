160ee79817df80118da6760477e437e48f59ae40
JPesel <jacobo@pesel.org>
2021-11-18 12:21:00 -0600

    adding remaining code and images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,7 @@ lmMC<- lm(mpg~.,dataMC)
 summary(lmMC)
 
 
+dataSC<- read.csv('Suspension_Coil.csv')
+attach(dataSC)
+total_summary<- summarize(dataSC, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary<- dataSC %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
